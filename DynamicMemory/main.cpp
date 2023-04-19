4486b66dfac1752f1be4739afbabfbe6ad3443e6
1 <1@DESKTOP-NU9I6RS>
2023-04-19 23:00:52 +0800

    new pointers

---
+++ DynamicMemory/main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+void FillRand(int arr[], const int n);
+void Print(int arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	int n = 5;
+	cout << "¬ведите размер массива: "; cin >> n;
+	int* arr = new int[n];
+	FillRand(arr, n);
+	Print(arr, n);
+	delete[] arr;
+
+}
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		*(arr + i) = rand() % 100;
+	}
+}
+void Print(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
--- TransferParameters/main.cpp
+++ TransferParameters/main.cpp
@@ -2,7 +2,9 @@
 #include<iostream>
 using namespace std;
 
-void Exchange(int a, int b);
+#define delimiter "\n___________________________________\n"
+
+void Exchange(int& a, int& b);
 
 void main()
 {
@@ -10,12 +12,14 @@ void main()
 	int a = 2, b = 3;
 	cout << a << "\t\t" << b << endl;
 	cout << &a << "\t" << &b << endl;
+	cout << delimiter << endl;
 	Exchange(a, b);
+	cout << delimiter << endl;
 	cout << a << "\t\t" << b << endl;
 	cout << &a << "\t" << &b << endl;
 }
 
-void Exchange(int a, int b)
+void Exchange(int& a, int& b)
 {
 	cout << &a << "\t" << &b << endl;
 	int buffer = a;
